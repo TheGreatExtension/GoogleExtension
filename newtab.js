69f143732878c51f83ff58cf4f0323bb3d739d51
Elena6918 <Elena000623@gmail.com>
2020-03-29 15:45:40 -0400

    fix remove button and add tags

--- newtab.js
+++ newtab.js
@@ -23,56 +23,90 @@ function update(){
             currEvent = tempEvent[0][i];
             var t = currEvent[0].split("T");
             var s = "";
-            var time = s.concat(t[0], " ", t[1])
-            addEvent([time, currEvent[1]], index);
+            var time = s.concat(t[0], " ", t[1]);
+            var type = tempEvent[0][i][2];
+            var seq = i.toString();
+            addEvent([time, currEvent[1], type, seq]);
         }
     });
 }
 
 document.getElementById("button").onclick = function () {
-    document.getElementById('insertContent').innerHTML = "";   //clear
-    tempEvent = []
-    var time = document.getElementById("meeting-time").value;
-    pairs.push([time, document.getElementById("description").value]);
-    update();
+    if(pairs.length <= 9){
+        document.getElementById('insertContent').innerHTML = "";   //clear
+        tempEvent = []
+        var time = document.getElementById("meeting-time").value;
+        var seq = pairs.length.toString();
+        if(document.getElementById('exam').checked){
+            pairs.push([time, document.getElementById("description").value,"EXAM",seq]);
+        }
+        else if(document.getElementById('ddl').checked){
+            pairs.push([time, document.getElementById("description").value,"DEADLINE",seq]);
+        }
+        else if(document.getElementById('et').checked){
+            pairs.push([time, document.getElementById("description").value,"EVENT",seq]);
+        }
+        else if(document.getElementById('ot').checked){
+            pairs.push([time, document.getElementById("description").value,"OTHER",seq]);
+        }
+        update();
+    }
+    if(pairs.length == 10){
+        if(document.getElementById("closeAlert")==null){
+            let div = document.createElement('div');
+            div.className = "alert";
+            div.innerHTML = "<strong>Hi there!</strong> Prioritize 10 Tasks!";
+            let span = document.createElement('span');
+            span.className = "closebtn";
+            span.id = "closeAlert"
+            span.innerHTML = "&times";
+            document.getElementById("demo").append(div);
+            div.appendChild(span);
+            document.getElementById("closeAlert").onclick=function(){
+                document.getElementById("closeAlert").parentElement.style.display='none';
+            } 
+        }
+    }
 } 
 
 
-function addEvent(pair, id) {
+function addEvent(pair) {
     var empTab = document.getElementById('insertContent');
     var rowCnt = empTab.rows.length; 
     var tr = empTab.insertRow(rowCnt);    // TABLE ROW.
     tr = empTab.insertRow(rowCnt); 
-    for (var c = 0; c < 3; c++) { 
-    var td = document.createElement('td');   
-    td = tr.insertCell(c);
-    if(c==0){
-        var ele = document.createElement('input');
-        ele.setAttribute('value', pair[0]);
-        td.appendChild(ele);
-    }
-    else if(c==1){
-        var ele = document.createElement('input');
-        ele.setAttribute('value', pair[1]);
-        td.appendChild(ele);
-    }
-    else{
-        var button = document.createElement('input');
-        button.setAttribute('type', 'button');
-        button.setAttribute('value', 'Remove');
-        rid = "event"+id;
-        button.setAttribute('id',rid)
-        td.appendChild(button);
-        document.getElementById(rid).onclick = function () {
-            removeRow(document.getElementById(rid));
+    for (var c = 0; c < 4; c++) { 
+        var td = document.createElement('td');   
+        td = tr.insertCell(c);
+        if(c==0){
+            var ele = document.createElement('input');
+            ele.setAttribute('value', pair[0]);
+            td.appendChild(ele);
         }
-        
-        
-    }
+        else if(c==1){
+            var ele = document.createElement('input');
+            ele.setAttribute('value', pair[1]);
+            td.appendChild(ele);
+        }
+        else if(c==2){
+            var tag = document.createElement('input');
+            console.log(pair[2])
+            tag.setAttribute('value', pair[2]);
+            td.appendChild(tag);
+        }
+        else{
+            var span = document.createElement('span');
+            span.className = "closebtn";
+            span.setAttribute("id", "event"+pair[3])
+            span.innerHTML = "&times";
+            td.appendChild(span);
+            id = "event"+pair[3];
+            span.onclick=function(){
+                removeRow(this);
+            }
+        }
     }
 }
-
-console.log(document.getElementById("event0"));
 
 
 function addLine(){
@@ -83,7 +117,6 @@ function addLine(){
 function removeRow(oButton) {
     id = oButton.id;
     rowNum = id.split('t')[1];
-    console.log("rownum: "+rowNum);
     var empTab = document.getElementById('insertContent');
     empTab.deleteRow(rowNum);       // BUTTON -> TD -> TR.
     chrome.storage.local.clear(function(){
@@ -93,7 +126,6 @@ function removeRow(oButton) {
     }
     });
     pairs.splice(rowNum,1)
-    // console.log("after remove: "+pairs);
     update();
 }
 
@@ -106,6 +138,7 @@ function sortTime(a,b){
     }
 }
 
+//background image change
 var images = [
     "b7.jpg","b8.jpg","b6.jpg",'b1.jpg','b2.jpg','b3.jpg',
     'b4.jpg','b5.jpg','b9.jpg','b10.jpg','b11.jpg','b12.jpg'
@@ -120,4 +153,4 @@ setInterval(function() {
       if (i == images.length) {
       	i =  0;
       }
-}, 3000);
+}, 30000);
